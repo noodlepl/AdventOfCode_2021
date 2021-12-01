74742a3fbbe2988f6c093c3c5f07b430b1785440
Adam Kępiński <adam.kepinski@tomtom.com>
2021-12-01 19:04:03 +0100

    Task 1 part 2

--- 01.py
+++ 01.py
@@ -11,3 +11,18 @@ with open('01.txt', 'r') as file:
         prev = x
 
     print(res)
+
+    #part 2
+
+with open ('01.txt', 'r') as file:
+    data = [int(line) for line in file]
+
+    res = 0
+
+    prev = sum(data[0:3])
+    for idx, value in enumerate(data[1:-2], start=1):
+        current = data[idx] + data[idx+1] + data[idx+2]
+        if (current > prev):
+            res = res + 1
+        prev = current
+    print(res)
