5cb741e6b0e8643e73ce7e7030dc681649059943
Adam Kępiński <adam.kepinski@tomtom.com>
2021-12-02 11:43:22 +0100

    Task 2 part 2 + different data sets to use the same account as last year

--- 02.py
+++ 02.py
@@ -5,7 +5,6 @@ with open('02.txt', 'r') as file:
     horizontal = 0
     depth = 0
     data = [tuple(line.split()) for line in file]
-    print(data)
 
     for instruction in data:
         direction = instruction[0]
@@ -16,6 +15,29 @@ with open('02.txt', 'r') as file:
             depth = depth - value
         elif direction == "down":
             depth = depth + value
+        else:
+            print("unknown direction")
+
+    print(horizontal * depth)
+
+# part 2
+
+with open('02.txt', 'r') as file:
+    horizontal = 0
+    depth = 0
+    aim = 0
+    data = [tuple(line.split()) for line in file]
+
+    for instruction in data:
+        direction = instruction[0]
+        value = int(instruction[1])
+        if direction == "forward":
+            horizontal = horizontal + value
+            depth = depth + aim * value
+        elif direction == "up":
+            aim = aim - value
+        elif direction == "down":
+            aim = aim + value
         else:
             print("unknown direction")
 
