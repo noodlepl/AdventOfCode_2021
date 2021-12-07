b654adbe68e257b7fb1e91b64e94b8e885a14d03
Adam Kępiński <adam.kepinski@tomtom.com>
2021-12-07 08:21:14 +0100

    Task 7 part 2

--- 07.py
+++ 07.py
@@ -12,6 +12,23 @@ with open('07.txt', 'r') as file:
         for pos in positions:
             fuel_needed += abs(pos - common_pos)
         fuels.append(fuel_needed)
-        print("for common pos {} we need {} fuel".format(common_pos, fuel_needed))
+
+    print(min(fuels))
+
+#part 2
+
+with open('07.txt', 'r') as file:
+    positions = [int(x) for x in file.readline().strip().split(',')]
+    print(positions)
+
+    start = min(positions)
+    stop = max(positions)+1
+    fuels = []
+    for common_pos in range(start, stop):
+        fuel_needed = 0
+        for pos in positions:
+            dist = abs(pos-common_pos)
+            fuel_needed += int((1+dist)/2*dist)
+        fuels.append(fuel_needed)
 
     print(min(fuels))
